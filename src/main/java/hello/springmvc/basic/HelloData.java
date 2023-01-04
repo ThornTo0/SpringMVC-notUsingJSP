23ff6f3097c49edd30b5a6819449de03760003ab
To <tok1224@naver.com>
2023-01-04 10:42:03 +0900

    요청 매핑 - 요청 바디로 전송하고 읽기

---
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Data
+public class HelloData {
+
+    private String username;
+    private int age;
+}
